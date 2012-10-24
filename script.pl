39c5d86d8f4c70bcefd3b9cc537e3ae80c0b1846
Arjun <arjun@rdaemon.com>
2012-10-23 22:03:37 -0400

    partial updates

---
+++ script.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use vars qw($VERSION %IRSSI);
+
+#use Irssi;
+$VERSION = '1.0.0';
+%IRSSI  = (
+	authors => 'Mallikarjun',
+	contact => 'arjun@rdaemon.com',
+	name => 'irssi-notify',
+	description => 'Ubuntu notifications'.
+								 ' for irssi',
+	license => 'BSD',
+	url => 'http://www.google.com',
+	changed => 'Sun Jan 20 2002',
+);
